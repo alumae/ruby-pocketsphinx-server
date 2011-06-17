263cefa1140124bf66f100de2332ad54847b61cb
Tanel Alumae <tanel@viterbi.kybi>
2011-06-17 14:33:48 +0300

    format change works

--- raw_recognizer.rb
+++ raw_recognizer.rb
@@ -13,16 +13,43 @@ class Recognizer
 	def initialize()
 		@clock = Gst::SystemClock.new
 		@result = ""
-		# construct pipeline
-		@pipeline = Gst::Parse.launch("appsrc name=appsrc ! decodebin2 ! audioconvert ! audioresample ! pocketsphinx name=asr ! appsink name=appsink")
+		## construct pipeline
+		#@pipeline = Gst::Parse.launch("appsrc name=appsrc ! decodebin2 ! audioconvert ! audioresample ! pocketsphinx name=asr ! appsink name=appsink")
 		
-		# define input audio properties
-		@appsrc = @pipeline.get_child("appsrc")
-		caps = Gst::Caps.parse("audio/x-flac, rate=(int)16000;")
-		@appsrc.set_property("caps", caps)
+		## define input audio properties
+		#@appsrc = @pipeline.get_child("appsrc")
+		#caps = Gst::Caps.parse("audio/x-flac, rate=(int)16000;")
+		#@appsrc.set_property("caps", caps)
 		
-		# define behaviour for ASR output
-		@asr = @pipeline.get_child("asr")
+		## define behaviour for ASR output
+		#@asr = @pipeline.get_child("asr")
+
+
+		@appsrc = Gst::ElementFactory.make "appsrc", "appsrc"
+		@decoder = Gst::ElementFactory.make "decodebin2", "decoder"
+		@audioconvert = Gst::ElementFactory.make "audioconvert", "audioconvert"
+		@audioresample = Gst::ElementFactory.make "audioresample", "audioresample"
+		@asr = Gst::ElementFactory.make "pocketsphinx", "asr"
+		@appsink = Gst::ElementFactory.make "appsink", "appsink"
+
+		#caps = Gst::Caps.parse("audio/x-flac, rate=(int)16000;")
+		#@appsrc.set_property("caps", caps)
+
+    create_pipeline()
+		
+		#@pipeline.pause
+	end
+
+	def create_pipeline()
+		@pipeline = Gst::Pipeline.new "pipeline"
+		@pipeline.add @appsrc, @decoder, @audioconvert, @audioresample, @asr, @appsink
+		@appsrc >> @decoder		
+		@audioconvert >> @audioresample >> @asr >> @appsink
+		
+		@decoder.signal_connect('pad-added') { | element, pad, last, data | 
+			puts "---- pad-added ---- "
+			pad.link  @audioconvert.get_pad("sink")
+	  }
 
 		
 		@queue = Queue.new
@@ -46,14 +73,20 @@ class Recognizer
 		@appsink.signal_connect('eos') { |appsink, data|
 				puts "##### EOS #####"
 		}
-		
-		#@pipeline.pause
-	end
 
+		@bus = @pipeline.bus
+		@bus.signal_connect('message::state-changed') { |appsink, data|
+				puts "##### STATE-CHANGED #####"
+		}
+  end	
  
     
   # Call this before starting a new recognition
-  def clear
+  def clear(caps)
+		#caps = Gst::Caps.parse(caps)
+		#@appsrc.set_property("caps", caps)
+
+  
     @result = ""
     queue.clear
     pipeline.pause
@@ -77,7 +110,13 @@ class Recognizer
   # Returns the final recognition result
   def wait_final_result
     queue.pop
-    @appsrc.ready
+    @pipeline.stop
+
     return result
   end  
+  
+  def stop
+    @pipeline.play
+    @pipeline.stop
+	end
 end
--- sinatra-chunks.rb
+++ sinatra-chunks.rb
@@ -1,11 +1,9 @@
 require 'sinatra'
-
-#require '../playground/speech-api/speech-api'
 require 'raw_recognizer'
 require 'uuidtools'
 require 'json'
 require 'iconv'
-#require 'gst'
+require 'set'
 
 configure do
 	RECOGNIZER = Recognizer.new
@@ -15,21 +13,50 @@ post '/' do
   id = SecureRandom.hex
   puts "Request ID: " + id
   req = Rack::Request.new(env)
-  puts "Parsing content type", req.content_type
-  caps = Gst::Caps.parse(req.content_type)
+  puts "Parsing content type " + req.content_type
+  caps_str = content_type_to_caps(req.content_type)
+  puts "CAPS string is " + caps_str
+  caps = Gst::Caps.parse(caps_str)
   RECOGNIZER.appsrc.set_property("caps", caps)  
-  RECOGNIZER.clear()
+  RECOGNIZER.clear(caps)
+  
+  length = 0
   req.body.each do |chunk|
     print "."
     RECOGNIZER.feed_data(chunk)   
+    length += chunk.size
   end
   puts "Got feed end"
-  RECOGNIZER.feed_end()
-  result = RECOGNIZER.wait_final_result()
-  puts "RESULT:" + result
-                        
-  result = Iconv.iconv('utf-8', 'latin1', result)
-  
-  headers "Content-Type" => "application/json; charset=utf-8", "Content-Disposition" => "attachment"
-  JSON.pretty_generate({:status => 0, :id => id, :hypotheses => [:utterance => result]})
+  if length > 0
+		RECOGNIZER.feed_end()
+		result = RECOGNIZER.wait_final_result()
+		puts "RESULT:" + result
+													
+		result = Iconv.iconv('utf-8', 'latin1', result)
+		
+		headers "Content-Type" => "application/json; charset=utf-8", "Content-Disposition" => "attachment"
+		JSON.pretty_generate({:status => 0, :id => id, :hypotheses => [:utterance => result]})
+	else
+	  RECOGNIZER.stop
+		headers "Content-Type" => "application/json; charset=utf-8", "Content-Disposition" => "attachment"
+		JSON.pretty_generate({:status => 0, :id => id, :hypotheses => [:utterance => ""]})
+	end
 end
+
+def content_type_to_caps(content_type)
+  if not content_type
+		return "audio/x-raw-int,rate=16000,channels=1,signed=true,endianness=1234,depth=16,width=16"
+	end
+  parts = content_type.split(";")
+  result = ""
+  allowed_types = Set.new ["audio/x-flac", "audio/x-raw-int", "application/ogg", "audio/mpeg"]
+  if allowed_types.include? parts[0]
+    result = parts[0]
+    if parts[0] == "audio/x-raw-int"
+      result = ",".join(parts)
+     end
+  end
+	return result
+end
+
+
