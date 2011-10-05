1c54cc1e97e15cb86d6f3a2fea2719dec64aebf0
Tanel Alumae <tanel@viterbi.kybi>
2011-10-05 18:37:51 +0300

    JSGF->FST>OpenFST->FSG

---
+++ scripts/jsgf2fsg.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+if [ $# -ne 2 ]
+then
+  echo "Usage: `basename $0` jsgf fsg"
+  exit 1
+fi
+
+sphinx_jsgf2fsg -jsgf $1 -fsm ${1%.*}.fsm  -symtab ${1%.*}.sym
+
+fstcompile --arc_type=log --acceptor --isymbols=${1%.*}.sym --keep_isymbols ${1%.*}.fsm | \
+	fstdeterminize | fstminimize | fstrmepsilon   | fstpush --push_weights  | fstprint | \
+	`dirname $0`/fsm2fsg.py > $2
