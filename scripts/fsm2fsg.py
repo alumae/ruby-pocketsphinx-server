261cbd98db4a8e9bd9bfaa9a429dc8281966b8c2
Tanel Alumae <tanel@viterbi.kybi>
2011-10-06 13:26:59 +0300

    Improve FSG-based recognition

--- scripts/fsm2fsg.py
+++ scripts/fsm2fsg.py
@@ -5,15 +5,17 @@ import sys
 from math import exp
 
 if __name__ == '__main__':
+  #HACK: currently all weights are set to 1. Seems to make the recognition more robust
+  
   arcs = []
   for l in sys.stdin:
     ss = l.split()
     if len(ss) == 5:
-      arcs.append((int(ss[0]), int(ss[1]), ss[2], exp((-float(ss[4])))))
+      arcs.append((int(ss[0]), int(ss[1]), ss[2], 1)
     elif len(ss) == 4:
       arcs.append((int(ss[0]), int(ss[1]), ss[2], 1))
     elif len(ss) == 2:
-      arcs.append((int(ss[0]), -1, "", exp(-(float(ss[1])))))
+      arcs.append((int(ss[0]), -1, "", 1)
     elif len(ss) == 1:
       arcs.append((int(ss[0]), -1, "", 1))
     else:
@@ -23,9 +25,6 @@ if __name__ == '__main__':
   
   final_state_id = max_state + 1
   
-  # HACK: make all weights 1.0. This is stupid, but seems to make the recognition more robust
-  for a in arcs:
-    a = (a[0], a[1], a[2], 1.0)
   
   print "FSG_BEGIN <test>"
   print "NUM_STATES", max_state + 2
