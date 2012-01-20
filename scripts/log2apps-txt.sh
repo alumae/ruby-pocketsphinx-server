c6b618508bd573d838e57592c98f7bed676ecd76
Tanel Alumae <alumae@gmail.com>
2012-01-20 11:27:46 +0200

    Small bug

--- scripts/log2apps-txt.sh
+++ scripts/log2apps-txt.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-egrep "^I, \[.*INFO.*User agent.* .*\)" $1 | perl -npe 's/.* (\S+)\)/\1/' | sort | uniq -c | sort -nr
+egrep "^I, \[.*INFO.*User agent.* \(RecognizerIntentActivity.*\)" $1 | perl -npe 's/.* (\S+)\)/\1/' | sort | uniq -c | sort -nr
