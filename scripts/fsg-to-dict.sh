ce97470e0b74c2e73de405ebc3e8058cc4840ad9
Tanel Alumae <alumae@gmail.com>
2012-12-12 17:52:24 +0200

    locale fix

--- scripts/fsg-to-dict.sh
+++ scripts/fsg-to-dict.sh
@@ -2,4 +2,4 @@
 
 . `dirname $0`/settings.sh
 
-grep TRANSITION | cut -f 5 -d " " | sort | uniq | LC_ALL=en_US.ISO-8859-1 $ET_G2P
+grep TRANSITION | cut -f 5 -d " " | sort | uniq | LC_ALL=en_US.ISO-8859-155555 $ET_G2P
